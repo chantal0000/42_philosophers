9aa489e15cb9d3b22d047faf6133516a1ef448fc
Chantal Bürger <cb@cb-0206.fritz.box>
2024-08-21 16:23:00 +0200

    pseudocode routine

---
+++ trial003/routine.c
@@ -0,0 +1,29 @@
+
+
+#include 'philo.h'
+
+// starting point of simulation: / start_dinner
+
+// save time
+// create thread to monitor everything
+// create thread for each phil and pass the routine
+// join all threads
+// join monitor thread
+
+
+
+// routine
+
+// time which group starts when with eating (avoid deadlock)
+
+// while loop that checks for dead philosophers
+// philo_eating fct
+// schlafen -> print msg & wartet bis aufwachen
+// thinkjing -> print msg & wait for finish
+// loop goes forever unless someone dies
+
+
+// ft_eat
+// figure out order in which they take the forks
+// case if only one phil
+// 
