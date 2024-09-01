0d12516882c069e92e848b86070369bfc67ed95a
Chantal Munja Buerger <c_buerger@gmx.net>
2024-09-01 10:34:11 +0200

    working, all tests with helgrind and fsanitize, last philo grabs opposite order and 1000 ms added to thinking process

--- trial004/routine.c
+++ trial004/routine.c
@@ -6,7 +6,7 @@
 /*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 09:51:24 by chbuerge          #+#    #+#             */
-/*   Updated: 2024/08/31 17:17:27 by chbuerge         ###   ########.fr       */
+/*   Updated: 2024/09/01 10:27:41 by chbuerge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,41 @@
 
 int	ft_eat(t_philo *philo)
 {
+	if (philo->id == philo->table->nb_of_philos && philo->table->nb_of_philos != 1)
+	{
+		pthread_mutex_lock(philo->m_right_fork);
+	// printf("left: %p\n",  (void*)philo->m_left_fork);
+	if (ft_print(philo, "has taken a fork") == 1)
+	{
+		pthread_mutex_unlock(philo->m_right_fork);
+		return (1);
+	}
+
+	pthread_mutex_lock(philo->m_left_fork);
+	// printf("right: %p\n",  (void*)philo->m_right_fork);
+	if (ft_print(philo, "has taken a fork") == 1)
+	{
+		pthread_mutex_unlock(philo->m_right_fork);
+		pthread_mutex_unlock(philo->m_left_fork);
+		return (1);
+	}
+	if (ft_print(philo, "is eating") == 1)
+	{
+		pthread_mutex_unlock(philo->m_right_fork);
+		pthread_mutex_unlock(philo->m_left_fork);
+		return (1);
+	}
+	pthread_mutex_lock(&philo->table->m_timestamp);
+	philo->timestamp_last_meal = ft_timestamp();
+	pthread_mutex_unlock(&philo->table->m_timestamp);
+
+	usleep(philo->table->time_to_eat * 1000);
+
+	pthread_mutex_unlock(philo->m_right_fork);
+	pthread_mutex_unlock(philo->m_left_fork);
+	}
+	else
+	{
 	pthread_mutex_lock(philo->m_left_fork);
 	// printf("left: %p\n",  (void*)philo->m_left_fork);
 	if (ft_print(philo, "has taken a fork") == 1)
@@ -48,6 +83,7 @@ int	ft_eat(t_philo *philo)
 
 	pthread_mutex_unlock(philo->m_left_fork);
 	pthread_mutex_unlock(philo->m_right_fork);
+	}
 	philo->nb_of_meals_eaten++;
 	if (philo->nb_of_meals_eaten == philo->table->nb_of_meals)
 	{
@@ -72,6 +108,8 @@ int	ft_think(t_philo *philo)
 	if (read_dead_flag(philo) == false)
 	{
 		ft_print(philo, "is thinking");
+		// usleep(((philo->table->time_to_die - (philo->table->time_to_eat + philo->table->time_to_sleep)) / 2));
+		usleep(1000);
 		return (0);
 	}
 	return (1);
